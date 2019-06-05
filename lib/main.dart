73b181d906a01577ab33ec61471f536aa4b41081
Gislainy Crisostomo Veslaco <gislainycrisostomo@gmail.com>
2019-06-04 22:31:16 -0300

    classresponsável por gerenciar o contato

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
  import 'package:flutter/material.dart';
+import 'package:flutter_agenda_contato/ui/home_page.dart';
 
 void main() => runApp(MaterialApp(
-  
+  home: HomePage()
 ));
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,24 @@
+
+import "package:flutter/material.dart";
+import 'package:flutter_agenda_contato/helpers/contact_helper.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  ContactHelper helper = ContactHelper();
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
